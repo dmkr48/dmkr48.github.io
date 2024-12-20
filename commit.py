cbdefc67fd2dbc5bc52de18aa3db016bc2fcd1fb
dmkr48 <difu27012011@gmail.com>
2024-12-20 23:22:20 +0700

    Update commit.py

--- commit.py
+++ commit.py
@@ -3,9 +3,9 @@ import os
 from git import Repo
 import datetime as dt
 tgl = dt.datetime.now().strftime("%y%m%d_%H%M%S")
-os.chdir('/content/pages')
+os.chdir('/content/dmkr48')
 
-full_local_path = "/content/pages"
+full_local_path = "/content/dmkr48"
 
 repo = Repo(full_local_path)
 repo.git.add("-A")
